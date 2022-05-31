5eb3df5fa38db66b7057aae85f0c53be4e691b4d
milaamir <Milana2001>
2022-06-01 00:12:47 +0300

    add ToureoApplication

---
+++ src/main/java/ru/itis/toureo/ToureoApplication.java
@@ -0,0 +1,15 @@
+package ru.itis.toureo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToureoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ToureoApplication.class, args);
+    }
+
+
+}
